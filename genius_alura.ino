78fe7000e2c139c8e5a8f10e419ee917cbba0ff3
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-01-12 08:41:42 -0300

    firstLed

---
+++ genius_alura.ino
@@ -0,0 +1,27 @@
+//  int ledVermelho = 2;
+#define LED_VERMELHO 2
+#define UM_SEGUNDO 1000
+#define MEIO_SEGUNDO 500
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_VERMELHO,OUTPUT);
+  digitalWrite(LED_VERMELHO,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // piscaLed(LED_VERMELHO);
+  
+}
+
+void piscaLed(int portaLed){
+
+  Serial.println(portaLed);
+  digitalWrite(portaLed,HIGH);
+  delay(UM_SEGUNDO);
+  digitalWrite(portaLed,LOW);
+  delay(MEIO_SEGUNDO);
+}
