ea3ca777ee088c17b07cd584b0fae1ab43fd6411
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-01-14 16:42:37 -0300

    PiscaTodosOsLeds

--- genius_alura.ino
+++ genius_alura.ino
@@ -1,19 +1,31 @@
 //  int ledVermelho = 2;
-#define LED_VERMELHO 2
+#define LED_VERDE 2
+#define LED_AMARELO 3
+#define LED_VERMELHO 4
+#define LED_AZUL 5
 #define UM_SEGUNDO 1000
 #define MEIO_SEGUNDO 500
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(LED_VERDE,OUTPUT);
+  pinMode(LED_AMARELO,OUTPUT);
   pinMode(LED_VERMELHO,OUTPUT);
-  digitalWrite(LED_VERMELHO,LOW);
+  pinMode(LED_AZUL,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  piscaLed(LED_VERMELHO);
+  // piscaLed(LED_VERDE);
+  // piscaLed(LED_AMARELO);
+  // piscaLed(LED_VERMELHO);
+  // piscaLed(LED_AZUL);
+  // delay(MEIO_SEGUNDO);
+
   
 }
 
