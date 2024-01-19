c5afe718f4686e9effd218fd6a1f168d7d6aae12
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-01-19 08:32:06 -0300

    botoes

--- genius_alura.ino
+++ genius_alura.ino
@@ -3,6 +3,13 @@
 #define LED_AMARELO 3
 #define LED_VERMELHO 4
 #define LED_AZUL 5
+#define INDEFINIDO -1
+
+#define BOTAO_VERDE 8
+#define BOTAO_AMARELO 9
+#define BOTAO_VERMELHO 10
+#define BOTAO_AZUL 11
+
 #define UM_SEGUNDO 1000
 #define MEIO_SEGUNDO 500
 
@@ -12,9 +19,9 @@ int sequenciaLuzes[TAMANHO_SEQUENCIA];
 
 void setup() {
   
-  // Serial.begin(9600);
-  // iniciaPortas();
-  // iniciaJogo();
+  Serial.begin(9600);
+  iniciaPortas();
+  iniciaJogo();
 }
 
 
@@ -23,13 +30,22 @@ void loop() {
   for(int i = 0; i <= TAMANHO_SEQUENCIA; i++){
     piscaLed(sequenciaLuzes[i]);
   }
+
+int botaoApertado = checaRespostaJogador();
+
+ Serial.println(botaoApertado);
 }
 
 void iniciaJogo(){
-  sequenciaLuzes[0] = LED_VERDE;
-  sequenciaLuzes[1] = LED_AMARELO;
-  sequenciaLuzes[2] = LED_VERMELHO;
-  sequenciaLuzes[3] = LED_AZUL;
+
+  for(int i = 0; i < TAMANHO_SEQUENCIA; i++){
+    sequenciaLuzes[i] = sorteiaCor();
+  }
+
+}
+
+int sorteiaCor(){
+  return random(LED_VERDE, LED_AZUL + 1);
 }
 
 void iniciaPortas(){
@@ -37,6 +53,36 @@ void iniciaPortas(){
   pinMode(LED_AMARELO,OUTPUT);
   pinMode(LED_VERMELHO,OUTPUT);
   pinMode(LED_AZUL,OUTPUT);
+
+  pinMode(BOTAO_VERDE, INPUT_PULLUP);
+  pinMode(BOTAO_AMARELO, INPUT_PULLUP);
+  pinMode(BOTAO_VERMELHO, INPUT_PULLUP);
+  pinMode(BOTAO_AZUL, INPUT_PULLUP);
+
+
+}
+
+int checaRespostaJogador(){
+
+  if(digitalRead(BOTAO_VERDE) == LOW){
+      return piscaLed(LED_VERDE);
+  }
+
+  if(digitalRead(BOTAO_AMARELO) == LOW){
+      return piscaLed(LED_AMARELO);
+  }
+
+  if(digitalRead(BOTAO_VERMELHO) == LOW){
+      return piscaLed(LED_VERMELHO);
+  }
+
+  if(digitalRead(BOTAO_AZUL) == LOW){
+      return piscaLed(LED_AZUL);
+  }
+
+  return INDEFINIDO;
+
+
 }
 
 
@@ -62,11 +108,13 @@ void piscaSequencia2(){
   digitalWrite(LED_AZUL,LOW);
 }
 
-void piscaLed(int portaLed){
+int piscaLed(int portaLed){
 
-  Serial.println(portaLed);
+  
   digitalWrite(portaLed,HIGH);
   delay(UM_SEGUNDO);
   digitalWrite(portaLed,LOW);
   delay(MEIO_SEGUNDO);
+
+  return portaLed;
 }
